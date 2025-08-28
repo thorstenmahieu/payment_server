7bdfa809f8365d4e51eecd88caa232134b636664
thorstenmahieu <thorstenmahieu@hotmail.com>
2025-08-28 19:55:03 +0200

    firstcommit

---
+++ payments.sql
@@ -0,0 +1,47 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "currency" (
+	"conversion_rate"	REAL NOT NULL,
+	"currency_name"	TEXT NOT NULL,
+	PRIMARY KEY("currency_name")
+);
+CREATE TABLE IF NOT EXISTS "payment_requests" (
+	"request_id"	INTEGER,
+	"requester_account_number"	TEXT NOT NULL,
+	"request_amount"	INTEGER NOT NULL,
+	"currency"	TEXT NOT NULL,
+	"request_time"	INTEGER NOT NULL,
+	"status"	TEXT NOT NULL,
+	PRIMARY KEY("request_id" AUTOINCREMENT),
+	FOREIGN KEY("currency") REFERENCES "currency"("currency_name"),
+	FOREIGN KEY("requester_account_number") REFERENCES "persons"("account_number")
+);
+CREATE TABLE IF NOT EXISTS "payments" (
+	"payment_id"	INTEGER,
+	"payment_amount"	REAL NOT NULL,
+	"payment_time"	INTEGER NOT NULL,
+	"payment_request_id"	INTEGER UNIQUE,
+	"payer_account_number"	INTEGER NOT NULL,
+	"currency"	TEXT NOT NULL,
+	PRIMARY KEY("payment_id" AUTOINCREMENT),
+	FOREIGN KEY("currency") REFERENCES "currency"("currency_name"),
+	FOREIGN KEY("payer_account_number") REFERENCES "persons"("account_number"),
+	FOREIGN KEY("payment_request_id") REFERENCES "payment_requests"("request_id")
+);
+CREATE TABLE IF NOT EXISTS "persons" (
+	"account_number"	TEXT,
+	"name"	TEXT NOT NULL,
+	PRIMARY KEY("account_number")
+);
+
+INSERT INTO "currency" (conversion_rate, currency_name) VALUES (1.0, 'USD');
+INSERT INTO "currency" (conversion_rate, currency_name) VALUES (0.85, 'EUR');
+INSERT INTO "currency" (conversion_rate, currency_name) VALUES (110.0, 'JPY');
+
+INSERT INTO "persons" (account_number, name) VALUES ('BE84 3410 1235 5486', 'Thorsten');
+INSERT INTO "persons" (account_number, name) VALUES ('BE84 5486 3410 1235', 'Maarten');
+INSERT INTO "persons" (account_number, name) VALUES ('BE84 3410 5486 1235', 'Mattias');
+
+INSERT INTO "payment_requests" (request_id, requester_account_number, request_amount, currency, request_time, status) VALUES (1, 'BE84 3410 1235 5486', 100, 'USD', 1622548800, 'PENDING');
+
+INSERT INTO "payments" (payment_id, payment_amount, payment_time, payment_request_id, payer_account_number, currency) VALUES (1, 100.0, 1622721600, 1, 'BE84 5486 3410 1235', 'USD');
+COMMIT;
